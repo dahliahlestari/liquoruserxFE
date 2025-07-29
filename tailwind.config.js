b02f482e236d2e44b226f1a249cb98b63ab0d5dd
Dahliahlestari <139366834+dahliahlestari@users.noreply.github.com>
2025-07-29 18:21:38 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        liquordark: "#1E1E1E",
+        liquorgold: "#D0AF5E",
+        liquordarkgold: "#88723F",
+        liquorgoldlight: "#C39E47",
+        // kamu bisa tambahkan sebanyak yang kamu mau
+      },
+    },
+  },
   plugins: [],
 };
